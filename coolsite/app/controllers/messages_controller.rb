e3215dfb3ffccf3b5694bcda59db2069fdb5ba47
amarean <marean.1@osu.edu>
2018-05-30 22:15:04 -0400

    mailer now working on contact page

--- coolsite/app/controllers/messages_controller.rb
+++ coolsite/app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
 
    		 if @message.valid?
     		  MessageMailer.contact_me(@message).deliver_now
-    		  redirect_to new_message, notice: "Message received"
+    		  redirect_to new_message_url, notice: "Message received"
    		 else
    		   render :new
    		 end
